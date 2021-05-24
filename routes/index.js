89aad85263464e2d66a170ad4fcc033c3ab7aaff
wei <a1234567045@yahoo.com.tw>
2021-05-24 19:54:43 +0800

    refactor: modify mongoose config

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+// 引入 todos 模組程式碼
+const todos = require('./modules/todos')
+
+
+module.exports = router
+
+
+router.use('/', home)
+// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
+router.use('/todos', todos)
+
+
